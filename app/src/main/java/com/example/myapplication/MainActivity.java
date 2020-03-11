c6a71a7f6e27ca71f18ea23171e246083061412d
马武甲 <2248029759@qq.com>
2020-03-11 21:44:18 +0800

    C4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         System.out.println(111);
         System.out.println(222);
+        System.out.println(444);
         setContentView(R.layout.activity_main);
     }
 }
